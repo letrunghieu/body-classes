452d112e6bd5623c36eb7607e6252089d1c334e5
Hieu Le <letrunghieu.cse09@gmail.com>
2015-11-16 10:46:11 +0700

    Unit test

---
+++ tests/BodyTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class BodyTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \HieuLe\BodyClasses\Body
+     */
+    private $_obj;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->_obj = new \HieuLe\BodyClasses\Body();
+    }
+
+    public function testAddClassesAsString()
+    {
+        $this->_obj->addClasses('foo');
+        $this->assertEquals(['foo'], $this->_obj->getClassArray());
+        $this->assertEquals('foo', $this->_obj->getClasses());
+    }
+
+    public function testAddClassesAsArray()
+    {
+        $this->_obj->addClasses(['bar', 'baz']);
+        $this->assertEquals(['bar', 'baz'], $this->_obj->getClassArray());
+        $this->assertEquals('bar baz', $this->_obj->getClasses());
+    }
+
+    public function testAddDuplicatedClasses()
+    {
+        $this->_obj->addClasses('foo');
+        $this->_obj->addClasses('bar');
+        $this->_obj->addClasses(['foo']);
+
+        $this->assertEquals(['foo', 'bar'], $this->_obj->getClassArray());
+        $this->assertEquals('foo bar', $this->_obj->getClasses());
+    }
+}
